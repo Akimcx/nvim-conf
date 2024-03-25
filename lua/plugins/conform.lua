806281140654d3bb7fffa0af1bad8cc8ea9f5901
Akim <silverdleo@protonmail.com>
2024-03-25 16:18:54 -0400

    chore(stylua): Prefer `single` to `double` quote

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,37 +1,44 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			-- Customize or remove this keymap to your liking
-			"<leader>cf",
-			function()
-				require("conform").format({ async = true, lsp_fallback = false })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
-	-- Everything in opts will be passed to setup()
-	opts = {
-		-- Define your formatters
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			javascript = { { "prettierd", "prettier" } },
-		},
-		-- Set up format-on-save
-		format_on_save = { timeout_ms = 500, lsp_fallback = false },
-		-- Customize formatters
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
-			},
-		},
-	},
-	init = function()
-		-- If you want the formatexpr, here is the place to set it
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+        {
+            -- Customize or remove this keymap to your liking
+            '<leader>cf',
+            function()
+                require('conform').format { async = true, lsp_fallback = false }
+            end,
+            mode = '',
+            desc = 'Format buffer',
+        },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            python = { 'isort', 'black' },
+            javascript = { { 'prettierd', 'prettier' } },
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500, lsp_fallback = false },
+        -- Customize formatters
+        formatters = {
+            shfmt = {
+                prepend_args = { '-i', '2' },
+            },
+            stylua = {
+                format = {
+                    defaultConfig = {
+                        quote_style = 'single',
+                    },
+                },
+            },
+        },
+    },
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
 }
